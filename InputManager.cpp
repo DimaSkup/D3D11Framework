76568499a98a4bcb6737bf703123e90382d0279f
unknown <dimaskupejko@gmail.com>
2021-10-07 18:22:15 +0300

    Framework v.1: "Input system #6: revising, finished; Rendering system #2: revising of a header file" main#7

--- InputManager.cpp
+++ InputManager.cpp
@@ -54,24 +54,24 @@ namespace D3D11Framework
 		case WM_KEYDOWN:
 			KeyIndex = static_cast<eKeyCodes>(wParam);
 			GetKeyboardState(lpKeyState);
-			ToUnicode(wParam,
-					HIWORD(lParam) & 0xFF,
-					lpKeyState,
-					buffer,
-					1,
-					0);
+			ToUnicode(wParam,			// the code of virtual key
+				HIWORD(lParam) & 0xFF,	// scan-code of the key
+				lpKeyState,				// a pointer to the 256-byte array which contains a keyboard current state
+				buffer,					// put here translated symbol or symbols Unicode
+				1,						// the buffer size
+				0);						// flags: 0 -- the menu is active
 			m_eventkey(KeyIndex, buffer[0], true);
 			break;
 
 		case WM_KEYUP:
 			KeyIndex = static_cast<eKeyCodes>(wParam);
 			GetKeyboardState(lpKeyState);
-			ToUnicode(wParam,
-					HIWORD(lParam) & 0xFF,
-					lpKeyState,
-					buffer,
-					1,
-					0);
+			ToUnicode(wParam,			// the code of virtual key
+				HIWORD(lParam) & 0xFF,	// scan-code of the key
+				lpKeyState,				// a pointer to the 256-byte array which contains a keyboard current state
+				buffer,					// put here translated symbol or symbols Unicode
+				1,						// the buffer size
+				0);						// flags: 0 -- the menu is active
 			m_eventkey(KeyIndex, buffer[0], false);
 			break;
 
@@ -126,8 +126,8 @@ namespace D3D11Framework
 		{
 			if (!(*it))
 				continue;
-
-			if ((*it)->MouseMove(MouseEvent(m_curx, m_cury)) == true)
+			// handle this event
+			else if ((*it)->MouseMove(MouseEvent(m_curx, m_cury)) == true)	
 				return;
 		}
 	}
@@ -143,207 +143,20 @@ namespace D3D11Framework
 			// mouse button is pressed
 			if (press == true)
 			{
+				// handle this event
 				if ((*it)->MousePressed(MouseEventClick(code, m_curx, m_cury)) == true)
 					return;
 			}
 			// mouse button is released
 			{
+				// handle this event
 				if ((*it)->MouseReleased(MouseEventClick(code, m_curx, m_cury)) == true)
 					return;
 			}
 		}
 	}
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include "stdafx.h"
-#include "InputCodes.h"
-#include "InputListener.h"
-#include "InputManager.h"
-#include "Log.h"
-
-namespace D3D11Framework
-{
-	void InputManager::Init()
-	{
-		m_MouseWheel = m_curx = m_cury = 0;
-		Log::Get()->Debug("Input Manager init");
-	}
-
-	void InputManager::Close()
-	{
-		if (!m_Listener.empty())
-			m_Listener.clear();
-
-		Log::Get()->Debug("Input Manager close");
-	}
-
-	// set the window zone
-	void InputManager::SetWinRect(const RECT &winrect)
-	{
-		m_windowrect.top = winrect.top;
-		m_windowrect.bottom = winrect.bottom;
-		m_windowrect.right = winrect.right;
-		m_windowrect.left = winrect.left;
-	}
-
-	// add a new listener
-	void InputManager::AddListener(InputListener* Listener)
-	{
-		m_Listener.push_back(Listener);
-	}
-
-	// get an event from Windows, do some action and notify the listeners
-	void InputManager::Run(const UINT &msg, WPARAM wParam, LPARAM lParam)
-	{
-		if (m_Listener.empty())
-			return;
-
-		eKeyCodes KeyIndex;
-		wchar_t buffer[1];
-		BYTE lpKeyState[256];
-
-		m_eventcursor();	// create a mouse moving event
-
-		switch(msg)
-		{
-		// KEYBUTTON events
-		case WM_KEYDOWN:
-			KeyIndex = static_cast<eKeyCodes>(wParam);
-			GetKeyboardState(lpKeyState);
-			ToUnicode(wParam,		// the code of virtual key
-				HIWORD(lParam) & 0xFF,	// scan-code of the key
-				lpKeyState,	// a pointer to the 256-byte array which contains a keyboard current state
-				buffer,		// put here translated symbol or symbols Unicode
-				1,			// the buffer size
-				0);			// flags: 0 -- the menu is active
-			m_eventkey(KeyIndex, buffer[0], true);
-			break;
-
-		case WM_KEYUP:
-			KeyIndex = static_cast<eKeyCodes>(wParam);
-			GetKeyboardState(lpKeyState);
-			ToUnicode(wParam,		// the code of virtual key
-					  HIWORD(lParam) & 0xFF,	// scan-code of the key
-					  lpKeyState,	// a pointer to the 256-byte array which contains a keyboard current state
-					  buffer,		// put here translated symbol or symbols Unicode
-					  1,			// the buffer size
-					  0);			// flags: 0 -- the menu is active
-			m_eventkey(KeyIndex, buffer[0], false);
-			break;
-
-		// LMB events
-		case WM_LBUTTONDOWN:
-			m_eventmouse(MOUSE_LEFT, true);
-			break;
-		case WM_LBUTTONUP:
-			m_eventmouse(MOUSE_LEFT, false);
-			break;
-
-		// RMB events
-		case WM_RBUTTONDOWN:
-			m_eventmouse(MOUSE_RIGHT, true);
-			break;
-		case WM_RBUTTONUP:
-			m_eventmouse(MOUSE_RIGHT, false);
-			break;
-
-		// MMB events
-		case WM_MBUTTONDOWN:
-			m_eventmouse(MOUSE_MIDDLE, true);
-			break;
-		case WM_MBUTTONUP:
-			m_eventmouse(MOUSE_MIDDLE, false);
-
-
-		// MOUSE WHEEL event
-		case WM_MOUSEWHEEL:
-			m_mousewheel((short)GET_WHEEL_DELTA_WPARAM(wParam) / WHEEL_DELTA);
-			break;
-		}
-	}
-
-	// create a mouse moving event
-	void InputManager::m_eventcursor()
-	{
-		POINT Position;
-		GetCursorPos(&Position);
-
-		Position.x -= m_windowrect.left;
-		Position.y -= m_windowrect.top;
-
-		if (m_curx == Position.x && m_cury == Position.y)
-			return;
-
-		m_curx = Position.x;
-		m_cury = Position.y;
-
-		for (auto it = m_Listener.begin(); it != m_Listener.end(); ++it)
-		{
-			if (!(*it))
-				continue;
-			else if ((*it)->MouseMove(MouseEvent(m_curx, m_cury)) == true)
-				return;
-		}
-	}
-
-	// create a mouse button clicking event
-	void InputManager::m_eventmouse(const eMouseKeyCodes code, bool press)
-	{
-		for (auto it = m_Listener.begin(); it != m_Listener.end(); ++it)
-		{
-			if (!(*it))
-				continue;
-
-			// mouse button is pressed
-			if (press == true)
-			{
-				if ((*it)->MousePressed(MouseEventClick(code, m_curx, m_cury)) == true)
-					return;
-			}
-			// mouse button is released
-			else
-			{
-				if ((*it)->MouseReleased(MouseEventClick(code, m_curx, m_cury)) == true)
-					return;
-			}
-		}
-	}
-
-	// create a mouse wheel rotating event
+	// create an event of mouse wheel rotating
 	void InputManager::m_mousewheel(short Value)
 	{
 		if (m_MouseWheel == Value)
@@ -355,14 +168,14 @@ namespace D3D11Framework
 		{
 			if (!(*it))
 				continue;
-
-			if ((*it)->MouseWheel(MouseEventWheel(m_MouseWheel, m_curx, m_cury)) == true)
+			// handle this event
+			else if ((*it)->MouseWheel(MouseEventWheel(m_MouseWheel, m_curx, m_cury)) == true)
 				return;
 		}
 	}
 
-	// create a keybutton event
-	void InputManager::m_eventkey(const eKeyCodes code, const wchar_t wc, bool press)
+	// create an event of keybutton clicking/releasing
+	void InputManager::m_eventkey(const eKeyCodes kc, const wchar_t wc, bool press)
 	{
 		for (auto it = m_Listener.begin(); it != m_Listener.end(); ++it)
 		{
@@ -372,19 +185,18 @@ namespace D3D11Framework
 			// keybutton is pressed
 			if (press == true)
 			{
-				if ((*it)->KeyPressed(KeyEvent(code, wc)) == true)
+				// handle this event
+				if ((*it)->KeyPressed(KeyEvent(kc, wc)) == true)
 					return;
 			}
 			// keybutton is released
 			else
 			{
-				if ((*it)->KeyReleased(KeyEvent(code, wc)) == true)
+				// handle this event
+				if ((*it)->KeyReleased(KeyEvent(kc, wc)) == true)
 					return;
 			}
 		}
 	}
 
-//---------------------------------------------------------
 }
-
-*/
--- Window.h
+++ Window.h
@@ -14,12 +14,12 @@ namespace D3D11Framework
 			height(480),
 			posx(200),
 			posy(20),
-			resizing(true)
+			resizing(true) 
 		{}
 
 		int posx;
 		int posy;
-		std::string caption;		// the window caption
+		std::wstring caption;		// the window caption
 		int width;					// the width of client part of the window
 		int height;					// the height of client part of the window
 		bool resizing;
@@ -34,19 +34,19 @@ namespace D3D11Framework
 
 		static Window* Get() { return m_wndthis; }
 
-		// Create a window
+		// create a window
 		bool Create(const DescWindow &desc);
 
-		// Window events handling
-		void RunEvent();
+		// window events handling
+		void RunEvent(void);
 
-		// Close the window
+		// close the window
 		void Close(void);
 
 		void SetInputManager(InputManager* inputManager);
 
-		HWND GetHWND() const { return m_hwnd; }
-		int Window::GetLeft() const { return m_desc.posx }
+		HWND Window::GetHWND() const { return m_hwnd; }
+		int Window::GetLeft() const { return m_desc.posx; }
 		int Window::GetTop() const { return m_desc.posy; }
 		int Window::GetWidth() const { return m_desc.width; }
 		int Window::GetHeight() const { return m_desc.height; }
@@ -58,7 +58,7 @@ namespace D3D11Framework
 		bool IsExit() const { return m_isexit; }
 		// notify about window activity
 		bool IsActive() const { return m_active; }
-		// notify about changes of the window
+		// notify about window changes
 		// warning: after calling notifies the window about event handling
 		bool IsResize()
 		{
@@ -67,7 +67,8 @@ namespace D3D11Framework
 			return ret;
 		}
 
-		LRESULT WndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam);
+		// events handling
+		LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 	private:
 		void m_UpdateWindowState();
@@ -75,18 +76,19 @@ namespace D3D11Framework
 		static Window* m_wndthis;
 
 		DescWindow m_desc;		// the window description
-		InputManager* m_inputManager;
+		InputManager* inputManager;
 		HWND m_hwnd;			// the window handler
-		bool m_isexit;			// flag which notifies about an exit event
+		bool m_isexit;			// the flag which notifies about the exit event
 		bool m_active;			// is the window active?
 		bool m_minimized;
 		bool m_maximized;
-		bool m_isresize;		// if the window change size
+		bool m_isresize;		// if the window changes its size
 	};
 
 
-	// event handler
-	static LRESULT CALLBACK StaticWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam);
+
+	// events handling
+	static LRESULT CALLBACK StaticWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 //----------------------------------------------------
 }
