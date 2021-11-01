db811004446ff69f4ccdbd1fe765bd6c98bb7c99
unknown <dimaskupejko@gmail.com>
2021-11-01 19:21:14 +0200

    Framework v.1: "Revising of the log system" main #15

--- Log.cpp
+++ Log.cpp
@@ -1,4 +1,4 @@
-// last revising at 31.10.21
+// last revising at 01.11.21
 
 #include "stdafx.h"
 #include "Log.h"
@@ -7,54 +7,49 @@
 
 namespace D3D11Framework
 {
-//-------------------------------------------------------------
-
-
 	Log* Log::m_instance = nullptr;
 
-	Log::Log()
+	Log::Log(void)
 	{
 		if (!m_instance)
 		{
-			m_file = nullptr;
 			m_instance = this;
+			m_file = nullptr;
 			m_init();
 		}
 		else
 		{
-			Err("The log file has already been created!");
+			Err("Log::Log(): the Log instance has already created");
 		}
 	}
 
-	Log::~Log()
+	Log::~Log(void)
 	{
 		m_close();
 		m_instance = nullptr;
 	}
 
-
-	void Log::m_init()
-	{
-		char timer[9];
-		char date[9];
-
-		_strtime_s(timer, 9);
-		_strdate_s(date, 9);
-
-
+	void Log::m_init(void)
+	{	
 		if (fopen_s(&m_file, LOGNAME, "w") == 0)
 		{
-			fprintf(m_file, "%s %s: The log file has been created successfully\n", timer, date);
-			fprintf(m_file, "-------------------------------------------------\n\n");
+			char timer[9];
+			char date[9];
+
+			_strtime_s(timer, 9);
+			_strdate_s(date, 9);
+
+			fprintf(m_file, "The beginning of the log file: %s %s\n", date, timer);
+			fprintf(m_file, "------------------------------------\n\n");
 		}
 		else
 		{
-			Err("%s %s: Error during creation of the log file...", timer, date);
+			printf("Log::m_init(): an error occured during creation of the log file\n");
 			m_file = nullptr;
 		}
 	}
 
-	void Log::m_close()
+	void Log::m_close(void)
 	{
 		if (!m_file)
 			return;
@@ -65,8 +60,8 @@ namespace D3D11Framework
 		_strtime_s(timer, 9);
 		_strdate_s(date, 9);
 
-		fprintf(m_file, "\n\n-------------------------------------\n");
-		fprintf(m_file, "%s %s: The end of the log file.\n", timer, date);
+		fprintf(m_file, "---------------------------------------\n");
+		fprintf(m_file, "The end of the log file: %s %s\n", date, timer);
 
 		fflush(m_file);
 		fclose(m_file);
@@ -81,21 +76,24 @@ namespace D3D11Framework
 		va_start(args, message);
 		len = _vscprintf(message, args) + 1;
 
-		buffer = new char[len];
-		assert(buffer);
+		buffer = new (std::nothrow) char[len];
 
-		vsprintf_s(buffer, len, message, args);
-		m_print("", buffer);
+		if (buffer)
+		{
+			vsprintf_s(buffer, len, message, args);
+			m_print("", buffer);
 
-		delete[] buffer;
-		buffer = nullptr;
+			delete[] buffer;
+			buffer = nullptr;
+		}
+
 		va_end(args);
 	}
 
 	void Log::Debug(const char* message, ...)
 	{
 #ifdef _DEBUG
-
+		
 		va_list args;
 		int len = 0;
 		char* buffer = nullptr;
@@ -103,19 +101,21 @@ namespace D3D11Framework
 		va_start(args, message);
 		len = _vscprintf(message, args) + 1;
 
-		buffer = new char[len];
-		assert(buffer);
+		buffer = new (std::nothrow) char[len];
 
-		vsprintf_s(buffer, len, message, args);
-		m_print("*DEBUG", buffer);
+		if (buffer)
+		{
+			vsprintf_s(buffer, len, message, args);
+			m_print("*DEBUG: ", buffer);
 
-		delete[] buffer;
-		buffer = nullptr;
+			delete[] buffer;
+			buffer = nullptr;
+		}
+
 		va_end(args);
 
 #endif
 	}
-
 
 	void Log::Err(const char* message, ...)
 	{
@@ -126,31 +126,55 @@ namespace D3D11Framework
 		va_start(args, message);
 		len = _vscprintf(message, args) + 1;
 
-		buffer = new char[len];
-		assert(buffer);
+		buffer = new (std::nothrow) char[len];
 
-		vsprintf_s(buffer, len, message, args);
-		m_print("*ERROR", buffer);
+		if (buffer)
+		{
+			vsprintf_s(buffer, len, message, args);
+			m_print("*ERROR: ", buffer);
 
-		delete[] buffer;
-		buffer = nullptr;
+			delete[] buffer;
+			buffer = nullptr;
+		}
+
 		va_end(args);
 	}
 
-
 	void Log::m_print(const char* levtext, const char* text)
 	{
-		time_t cl = clock();
+		clock_t clockTime = clock();
 		char timer[9];
 
-		printf("%s::%s: %s:%I64d", levtext, text, timer, cl);
+		_strtime_s(timer, 9);
+
+		printf("%s::%I64d: %s%s\n", timer, clockTime, levtext, text);
 
 		if (m_file)
 		{
-			fprintf(m_file, "%s::%s: %s:%I64d", levtext, text, timer, cl);
+			fprintf(m_file, "%s::%I64d: %s%s\n", timer, clockTime, levtext, text);
 			fflush(m_file);
 		}
 	}
-
-//-------------------------------------------------------------
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
